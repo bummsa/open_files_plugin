8ef9be0d472be224a0f78ee56edd549a688a68bd
ATCH <atch@swiss-as.com>
2024-05-28 14:49:41 +0200

    test 4

--- ios/Classes/OpenFilesPlugin.swift
+++ ios/Classes/OpenFilesPlugin.swift
@@ -18,17 +18,18 @@ public class SwiftOpenFilesPlugin: NSObject, FlutterPlugin {
 
     private func getOpenFileCount() -> Int {
         var openFileCount = 0
-        var name: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
-        var length = size_t(0)
-        sysctl(&name, 4, nil, &length, nil, 0)
-        let proc_kinfo = UnsafeMutablePointer<kinfo_proc>.allocate(capacity: length / MemoryLayout<kinfo_proc>.stride)
-        defer {
-            proc_kinfo.deallocate()
+        var fd: Int32 = 0
+        while true {
+            let res = fcntl(fd, F_GETFD)
+            if res == -1 {
+                if errno == EBADF {
+                    break
+                }
+            } else {
+                openFileCount += 1
+            }
+            fd += 1
         }
-        sysctl(&name, 4, proc_kinfo, &length, nil, 0)
-        let proc = proc_kinfo.pointee
-        let fileDesc = proc.kp_proc.p_fd
-        openFileCount = Int(fileDesc)
         return openFileCount
     }
 }
